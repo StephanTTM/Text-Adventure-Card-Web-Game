73df3431aa052b1b717336c19e615a114021ab33
StephanTTM <65316422+StephanTTM@users.noreply.github.com>
2025-08-16 00:46:22 -0400

    chore: remove unused React import

--- game-client/src/main.jsx
+++ game-client/src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { StrictMode } from 'react';
 import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </StrictMode>
 );
